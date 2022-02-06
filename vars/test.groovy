b344e5a8ef15ec9be978c3d703afd31d3164b397
jagadish <jagadesh.manchala@gmail.com>
2022-02-06 03:31:21 +0000

    commit 3

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,40 +2,39 @@ import groovy.sql.Sql
 import java.util.*
 import java.text.SimpleDateFormat 
 
-def dbinsert() {
 
+def dbinsert(String build_name,String user_name,String build_status,String start_date,String end_date){   
+  
     def dbURL = 'jdbc:mysql://localhost:3306/buildinfo'
     def dbUserName = 'root'
     def dbPassword = 'pol123@1982M'
     def dbDriver = 'com.mysql.cj.jdbc.Driver'
     def db = Sql.newInstance(dbURL,dbUserName,dbPassword,dbDriver)
     db.connection.autoCommit = false
-
- try {		
-   println args[0]
-   println args[1]
-   println args[2]
-   Date latestdate = new Date("05/11/2015");
-  //  def sqlstr="""INSERT INTO build (build_name,user_name, build_status,start_date,end_date) 
-   //       VALUES ('sample1','admin','success',NOW(),NOW())"""
-   
-   def build_name = args[0]
-   def user_name = args[1]
-   def build_status = args[2]
-   def start_date = args[3]
-   def end_date = args[4]
-		
+        
+//   def build_name = args[0]
+//   def user_name = args[1]
+//   def build_status = args[2]
+//   def start_date = args[3]
+//   def end_date = args[4]
+      
      def sqlstr="INSERT INTO build (build_name,user_name, build_status,start_date,end_date) VALUES ('$build_name','$user_name','$build_status', '$start_date','$end_date')"
 
     println sqlstr
+
+   try {
     db.execute(sqlstr);
     db.commit()
-   println("Successfully committed") 
-}catch(Exception e){
+    println("Successfully committed") 
+    }
+
+ catch(Exception e){
     db.rollback()
     println("Transaction rollback") 
 }finally{
      db.close()
 }
+   
 }
 
+dbinsert(args[0],args[1],args[2],args[3],args[4])
